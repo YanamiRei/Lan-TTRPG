c03221a8973b482a19d12efa41e01905f3536a17
Yanami Rei <rei.tteiimasu@gmail.com>
2025-12-26 23:51:29 +0530

    basic chat implementation

---
+++ scripts/Multiplayer.gd
@@ -0,0 +1,49 @@
+extends Control
+
+const PORT:int = 42069
+const IP_ADDRESS: String = "localhost"
+
+@onready var host_btn = $HostButton
+@onready var join_btn = $JoinButton
+@onready var usrnm = $Username
+@onready var send = $Send
+@onready var message = $Message
+@onready var messages = $Messages
+
+var username: String
+
+var msg: String
+
+
+func _on_host_button_pressed() -> void:
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(PORT)
+	get_tree().set_multiplayer(SceneMultiplayer.new(), self.get_path())
+	multiplayer.multiplayer_peer = peer
+	joined()
+
+
+func _on_join_button_pressed() -> void:
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(IP_ADDRESS, PORT)
+	get_tree().set_multiplayer(SceneMultiplayer.new(), self.get_path())
+	multiplayer.multiplayer_peer = peer
+	joined()
+
+
+
+func _on_send_pressed() -> void:
+	rpc("msg_rpc", username, message.text)
+
+@rpc("any_peer","call_local")
+
+func msg_rpc(uname, data):
+	messages.text += str(uname, ": ", data, "\n")
+	messages.scroll_vertical = INF
+	message.text = ""
+
+func joined():
+	host_btn.hide()
+	join_btn.hide()
+	usrnm.hide()
+	username = usrnm.text
